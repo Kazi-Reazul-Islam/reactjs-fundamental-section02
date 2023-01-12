26dc0d00a105ce51265889dfcd65a519492bb8e7
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-12 12:49:26 +0600

    export-from-json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from "react";
-import ReactHtmlParser from "react-html-parser";
+import exportFromJSON from "export-from-json";
 
 function App() {
-  let htmlString = "<h1>Hello</h1>";
-  return <div>{ReactHtmlParser(htmlString)}</div>;
+  const downLoadCSV = () => {
+    const data = [
+      { Name: "Reazul", Age: "23", Occupation: "Student", City: "Khulna" },
+      { Name: "Nazim", Age: "25", Occupation: "Teacher", City: "Dhaka" },
+      {
+        Name: "Shakib",
+        Age: "21",
+        Occupation: "Cricket Player",
+        City: "Barishal",
+      },
+      {
+        Name: "Dhony",
+        Age: "25",
+        Occupation: "Cricket Player",
+        City: "Dinajpur",
+      },
+      { Name: "Rubel", Age: "26", Occupation: "Student", City: "Others" },
+    ];
+    const fileName = "mycsv";
+    const exportType = exportFromJSON.types.csv;
+
+    exportFromJSON({ data, fileName, exportType });
+  };
+
+  return (
+    <div>
+      <button onClick={downLoadCSV}>Download CSV</button>
+    </div>
+  );
 }
 
 export default App;
