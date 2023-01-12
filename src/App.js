03065121b03c775cb21c88e3cf4c3f53717cdad2
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-12 12:07:23 +0600

    axios fileupload

--- src/App.js
+++ src/App.js
@@ -2,35 +2,28 @@ import React from "react";
 import axios from "axios";
 
 function App() {
-  const getList = () => {
-    let url = "";
+  const fileUpLoad = () => {
+    // Post Url
+    let url = "....";
+
+    //Body Form Data
+    let myFormData = new FormData();
+    myFormData.append("file1", "fileObj1");
+    myFormData.append("file2", "fileObj2");
+    myFormData.append("file3", "fileObj3");
+
+    // Header Configuration
+    let headerConfig = {
+      headers: { "content-type": "application/x-www-form-urlencoded" },
+    };
+
     axios
-      .get(url)
+      .post()
       .then((res) => {
         if (res.status === 200) {
           let data = res.data;
         } else {
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const postData = () => {
-    let url = "...";
-    let postBody = {
-      name: "Reazul",
-      city: "Khulna",
-      age: "23",
-    };
-
-    axios
-      .post(url, postBody)
-      .then((res) => {
-        if (res.status) {
-          let data = res.data;
-        } else {
+          //Something wrong
         }
       })
       .catch((err) => {
