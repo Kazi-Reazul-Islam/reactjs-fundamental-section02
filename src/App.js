f45bcab4c6ec211df0e6c75b55eb2f743ae60f6e
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-12 12:38:19 +0600

    react-html-parser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import React from "react";
-import cogoToast from "cogo-toast";
+import ReactHtmlParser from "react-html-parser";
 
 function App() {
-  const successToast = () => {
-    cogoToast.success("This is a success message!", {
-      position: "top-right",
-    });
-  };
-  const errorToast = () => {
-    cogoToast.error("This is a error message!", { position: "top-right" });
-  };
-  return (
-    <div>
-      <button className="btn btn-success" onClick={successToast}>
-        Success
-      </button>
-      <br />
-      <button className="btn btn-danger " onClick={errorToast}>
-        Error
-      </button>
-    </div>
-  );
+  let htmlString = "<h1>Hello</h1>";
+  return <div>{ReactHtmlParser(htmlString)}</div>;
 }
 
 export default App;
