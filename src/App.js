e3c66dc978c788880c26a1ae4655cd72a97bd005
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-14 00:33:30 +0600

    react-icon

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import localforage from "localforage";
 import React from "react";
+import { AiFillHome, AiFillAccountBook, AiFillAlert } from "react-icons/ai";
 
 function App() {
-  const setData = () => {
-    localforage.setItem("name", "Reazul Islam", (err) => {
-      if (err) {
-        alert(err);
-      } else {
-        alert("Data Save Success");
-      }
-    });
-  };
-  const getData = () => {
-    localforage.getItem("name", (err, value) => {
-      if (err) {
-        alert(err);
-      } else {
-        alert(value);
-      }
-    });
-  };
-  const removeData = () => {
-    localforage.removeItem("name", (err) => {
-      if (err) {
-        alert(err);
-      } else {
-        alert("Data Remove Success");
-      }
-    });
-  };
   return (
     <div>
-      <button onClick={setData} className="btn btn-success">
-        Set Data
+      <button className="btn btn-success">
+        <AiFillHome /> Set Data
       </button>
-      <button onClick={getData} className="btn btn-primary m-5">
-        Get Data
+      <button className="btn btn-primary m-5">
+        <AiFillAccountBook /> Get Data
       </button>
-      <button onClick={removeData} className="btn btn-danger ">
+      <button className="btn btn-danger ">
+        <AiFillAlert />
         Remove Data
       </button>
     </div>
