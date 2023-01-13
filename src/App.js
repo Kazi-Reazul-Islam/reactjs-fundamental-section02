6c6b4e2d95a0d51149cfd76b9e92b95a3739da12
Kazi Reazul Islam <kazireazulislam1999@gmail.com>
2023-01-13 23:44:40 +0600

    htmlToImage

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React from "react";
-import exportFromJSON from "export-from-json";
+import * as htmlToImage from "html-to-image";
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from "html-to-image";
 
 function App() {
-  const downLoadCSV = () => {
-    const data = [
-      { Name: "Reazul", Age: "23", Occupation: "Student", City: "Khulna" },
-      { Name: "Nazim", Age: "25", Occupation: "Teacher", City: "Dhaka" },
-      {
-        Name: "Shakib",
-        Age: "21",
-        Occupation: "Cricket Player",
-        City: "Barishal",
-      },
-      {
-        Name: "Dhony",
-        Age: "25",
-        Occupation: "Cricket Player",
-        City: "Dinajpur",
-      },
-      { Name: "Rubel", Age: "26", Occupation: "Student", City: "Others" },
-    ];
-    const fileName = "mycsv";
-    const exportType = exportFromJSON.types.csv;
+  const downLoadImage = () => {
+    var myClassTable = document.getElementById("classTable");
 
-    exportFromJSON({ data, fileName, exportType });
+    htmlToImage
+      .toPng(myClassTable)
+      .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
   };
-
   return (
     <div>
-      <button onClick={downLoadCSV}>Download CSV</button>
+      <div id="classTable" className="container">
+        <div className="row">
+          <div className="col">
+            <table>
+              <tr>
+                <td>Name</td>
+                <td>Roll</td>
+                <td>Class</td>
+                <td>City</td>
+              </tr>
+              <tr>
+                <td>Reazul</td>
+                <td>01</td>
+                <td>15</td>
+                <td>Khulna</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <button onClick={downLoadImage} className="btn btn-success">
+        Download Image
+      </button>
     </div>
   );
 }
